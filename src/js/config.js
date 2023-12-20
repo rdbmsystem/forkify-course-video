91257a4fb55b7502d9b943e5808696de2b6ba9a2
rdbmsystem <rdbmsystem@gmail.com>
2023-12-21 03:11:46 +0800

    Initial comming

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8d5977ff-bef5-4d8e-be3a-dae6390d1567';
+export const MODAL_CLOSE_SEC = 2.5;
